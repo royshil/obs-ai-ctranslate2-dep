dcc2199ea045ef517c07ec0937180737de4667a0
Tabby Cromarty <tabby@brightmoon.online>
2026-03-27 15:21:49 +0000

    Try again to fix quoting issues

--- build_ct2_linux.sh
+++ build_ct2_linux.sh
@@ -18,38 +18,81 @@ fi
 
 cd CTranslate2-$VERSION
 
-extra_cxx_flags="-fPIC"
-
-if [[ ${ACCEL} = "nvidia" ]]; then
-  additional_args="-DWITH_CUDA=ON -DWITH_CUDNN=OFF -DWITH_TENSOR_PARALLEL=OFF -DWITH_HIP=OFF"
-elif [[ ${ACCEL} = "amd" ]]; then
-  additional_args="-DWITH_CUDA=OFF -DWITH_CUDNN=OFF -DWITH_TENSOR_PARALLEL=OFF -DWITH_HIP=ON -DCMAKE_C_COMPILER=hipcc -DCMAKE_CXX_COMPILER=hipcc \"-DCMAKE_HIP_ARCHITECTURES=${AMD_GPU_TARGETS}\" \"-DGPU_TARGETS=${AMD_GPU_TARGETS}\""
-  extra_cxx_flags="-fPIC --no-warnings"
-else
-  additional_args="-DWITH_CUDA=OFF -DWITH_CUDNN=OFF -DWITH_TENSOR_PARALLEL=OFF -DWITH_HIP=OFF"
-fi
-
 function build_for_arch() {
   ARCH=$1
   echo "Building for ${ARCH}"
-  cmake . -B build_${ARCH}_${CONFIG}_${ACCEL} \
-    -DCMAKE_GENERATOR="Unix Makefiles" \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    -DOPENMP_RUNTIME=COMP \
-    -DWITH_ACCELERATE=OFF \
-    -DWITH_MKL=OFF \
-    -DWITH_DNNL=OFF \
-    -DWITH_OPENBLAS=ON \
-    -DWITH_RUY=OFF \
-    -DENABLE_CPU_DISPATCH=ON \
-    -DENABLE_PROFILING=OFF \
-    -DBUILD_CLI=OFF \
-    -DBUILD_TESTS=OFF \
-    -DCMAKE_BUILD_TYPE=${CONFIG} \
-    -DCMAKE_CXX_FLAGS="-fPIC" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    ${additional_args}
+
+  if [[ ${ACCEL} = "nvidia" ]]; then
+    cmake . -B build_${ARCH}_${CONFIG}_${ACCEL} \
+      -DCMAKE_GENERATOR="Unix Makefiles" \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+      -DOPENMP_RUNTIME=COMP \
+      -DWITH_ACCELERATE=OFF \
+      -DWITH_CUDA=ON \
+      -DWITH_CUDNN=OFF \
+      -DWITH_HIP=OFF \
+      -DWITH_MKL=OFF \
+      -DWITH_DNNL=OFF \
+      -DWITH_OPENBLAS=ON \
+      -DWITH_RUY=OFF \
+      -DWITH_TENSOR_PARALLEL=OFF \
+      -DENABLE_CPU_DISPATCH=ON \
+      -DENABLE_PROFILING=OFF \
+      -DBUILD_CLI=OFF \
+      -DBUILD_TESTS=OFF \
+      -DCMAKE_BUILD_TYPE=${CONFIG} \
+      -DCMAKE_CXX_FLAGS="-fPIC" \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+  elif [[ ${ACCEL} = "amd" ]]; then
+    cmake . -B build_${ARCH}_${CONFIG}_${ACCEL} \
+      -DCMAKE_GENERATOR="Unix Makefiles" \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+      -DOPENMP_RUNTIME=COMP \
+      -DWITH_ACCELERATE=OFF \
+      -DWITH_CUDA=OFF \
+      -DWITH_CUDNN=OFF \
+      -DWITH_HIP=ON \
+      -DWITH_MKL=OFF \
+      -DWITH_DNNL=OFF \
+      -DWITH_OPENBLAS=ON \
+      -DWITH_RUY=OFF \
+      -DWITH_TENSOR_PARALLEL=OFF \
+      -DENABLE_CPU_DISPATCH=ON \
+      -DENABLE_PROFILING=OFF \
+      -DBUILD_CLI=OFF \
+      -DBUILD_TESTS=OFF \
+      -DCMAKE_BUILD_TYPE=${CONFIG} \
+      -DCMAKE_CXX_FLAGS="-fPIC --no-warnings" \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+      -DCMAKE_C_COMPILER=hipcc \
+      -DCMAKE_CXX_COMPILER=hipcc \
+      -DCMAKE_HIP_ARCHITECTURES="${AMD_GPU_TARGETS}" \
+      -DGPU_TARGETS="${AMD_GPU_TARGETS}"
+  else
+    cmake . -B build_${ARCH}_${CONFIG}_${ACCEL} \
+      -DCMAKE_GENERATOR="Unix Makefiles" \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+      -DOPENMP_RUNTIME=COMP \
+      -DWITH_ACCELERATE=OFF \
+      -DWITH_CUDA=OFF \
+      -DWITH_CUDNN=OFF \
+      -DWITH_HIP=OFF \
+      -DWITH_MKL=OFF \
+      -DWITH_DNNL=OFF \
+      -DWITH_OPENBLAS=ON \
+      -DWITH_RUY=OFF \
+      -DWITH_TENSOR_PARALLEL=OFF \
+      -DENABLE_CPU_DISPATCH=ON \
+      -DENABLE_PROFILING=OFF \
+      -DBUILD_CLI=OFF \
+      -DBUILD_TESTS=OFF \
+      -DCMAKE_BUILD_TYPE=${CONFIG} \
+      -DCMAKE_CXX_FLAGS="-fPIC" \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+  fi
 
   cmake --build build_${ARCH}_${CONFIG}_${ACCEL} --config ${CONFIG} --parallel
   mkdir -p ../dist/${ARCH}/${CONFIG}-${ACCEL}
