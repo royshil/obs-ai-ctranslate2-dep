c7ec2f339cbcd20ca3fe12ab3197fe3b8c0f9c73
Tabby Cromarty <tabby@brightmoon.online>
2026-03-27 15:05:02 +0000

    Try to reduce warnings more

--- build_ct2_windows.ps1
+++ build_ct2_windows.ps1
@@ -41,7 +41,7 @@ if ($cudaBuild -ne $null) {
     "-DCMAKE_GENERATOR=`"Unix Makefiles`" " +
     "-DCMAKE_C_COMPILER='$env:HIP_PATH\bin\clang.exe' " +
     "-DCMAKE_CXX_COMPILER='$env:HIP_PATH\bin\clang++.exe' " +
-    "-DCMAKE_CXX_FLAGS=`"-Wno-deprecated -Wno-deprecated-declarations -Wno-deprecated-literal-operator -Wno-ignored-attributes -Wno-ignored-pragmas -Wno-unused-parameter -Wno-unused-result -Wno-unused-value -Wno-unused-variable -Wno-reorder-ctor`""
+    "-DCMAKE_CXX_FLAGS=`"--no-warnings`""
   $env:ROCM_PATH = $env:HIP_PATH
 } else {
   $accelFlag = "-DWITH_CUDA=OFF -DWITH_HIP=OFF"
